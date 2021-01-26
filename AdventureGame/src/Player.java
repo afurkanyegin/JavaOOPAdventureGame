4ad4f077a1f9ac393736696e1711d4053d3d6188
afurkanyegin <abdullahfurkanyegin@outlook.com>
2021-01-26 19:07:40 +0300

    2nd

--- AdventureGame/src/Player.java
+++ AdventureGame/src/Player.java
@@ -1,4 +1,48 @@
 
 public class Player {
-
+	private int damage,health,money;
+	private String name,cName;
+	Inventory inv;
+	public Player(String name) {
+		this.name = name;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public Inventory getInv() {
+		return inv;
+	}
+	public void setInv(Inventory inv) {
+		this.inv = inv;
+	}
+	
+	
+	
 }
